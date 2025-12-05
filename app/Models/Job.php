11d7ac888be9452b0cfce63f426f59013e4847f9
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-12-04 21:32:36 -0500

    Eloquent - added database

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,41 +1,10 @@
 <?php
 
-
-
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-          [
-              'id' => 1,
-              'title' => 'Director',
-              'salary' => '$100,000'
-          ],
-          [
-              'id' => 2,
-              'title' => 'Programmer',
-              'salary' => '$120,000'
-          ],
-          [
-              'id' => 3,
-              'title' => 'Support Eng',
-              'salary' => '$70,000'
-          ]
-       ];
-    }
+class Job extends Model{
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    }
 }
 
