79793129d60fa1574517f72f91974f3ead5fb87d
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-12-07 19:11:04 -0500

    Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model{
+    use HasFactory;
+
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this ->belongsTo(Employer::class);
+    }
+
 
 }
 
+
+// Understanding
+// post.php
+//     return this->belongsTo(user)
+//     return this->hasMany(comment)
+//
+// user.php
+//     return hasMany(post)
+//     return hasMany(comment)
+//
+// comments
+//     return this->belongsTo(user)
+//     return this->belongsTo(post)
