0bf6e0ff163217178e189ace57f092d11ced9c7e
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-12-14 14:33:13 -0500

    Patch and delete

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,49 @@ Route::post('/jobs', function(){
      return redirect('/jobs');
 });
 
+Route::get('/jobs/{id}/edit', function ($id){
+
+    $job = Job::find($id);
+
+    return view('jobs.edit ', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id){
+    // validate
+    request()->validate([
+        'title' => ['required'],
+        'salary' => ['required']
+    ]);
+
+    // authorize(on hold)
+
+
+    // update the job
+    $job =Job::findOrFail ($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' .$job->id);
+});
+
+//delete
+Route::delete('/jobs/{id}', function ($id){
+    // authorize the request(on hold)
+
+    // delete the job
+    Job::findOrFail ($id)->delete();
+    // redirect
+    return redirect('/jobs');
+
+
+});
+
+
 Route::get('/contact', function () {
     return view('contact');
 });
