2c14af7c1e846f898eb549ae0c51daed1cc72dfc
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-12-18 21:55:43 -0500

    auth

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class RegisteredUserController extends Controller
+{
+    public function create()
+    {
+        return view('auth/register');
+    }
+
+    public function store()
+    {
+       // validate
+        $attributes = request()->validate([
+            'first_name' =>['required'],
+            'last_name' =>['required'],
+            'email' =>['required', 'email'],
+            'password' =>['required','confirmed']
+        ]);
+
+       // Create the user
+       $user = User::create($attributes);
+
+       // Log in
+       Auth::login($user);
+
+       // redirect
+       return redirect('/jobs');
+    }
+}
---
+++ resources/views/Components/form-input.blade.php
@@ -0,0 +1,3 @@
+<div class="flex items-center rounded-md bg-white border border-gray-300 pl-3 focus-within:border-indigo-500">
+  <input {{ $attributes->merge(['class' => 'class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6']) }}
+</div>
